094898024683eb9b923c817048d5b03d77e828bd
JulioAsillo <u20201b482@upc.edu.pe>
2023-10-23 00:25:28 -0500

    add song model and service

---
+++ src/app/models/Song.ts
@@ -0,0 +1,8 @@
+import {Album} from "../../../../../../DESCARGASSSSSS/Team Software Process (TSP)/TSP TB3/tsp-fortlom-frontend-v1-develop/src/app/models/Album";
+
+export interface Song{
+  id: number;
+  name: string;
+  musicUrl: string;
+  album: Album;
+}
